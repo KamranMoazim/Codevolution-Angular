8181f20f849bf9819fcb44f79c7a33808e131eb7
Kamran Moazim <kamrannaseer765@gmail.com>
2024-05-20 15:49:19 +0500

    navbar completed

--- project/frontend/src/app/components/navbar/navbar.component.ts
+++ project/frontend/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,4 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -9,32 +7,27 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class NavbarComponent implements OnInit {
 
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
-  isMobile = true;
-  isCollapsed = false;
+  adminRoutes = [
+    { path: '/admin/dashboard', title: 'Dashboard' },
+    { path: '/admin/users', title: 'Users' },
+    { path: '/admin/roles', title: 'Roles' },
+    { path: '/admin/permissions', title: 'Permissions' },
+    { path: '/admin/roles-permissions', title: 'Roles Permissions' },
+    { path: '/admin/users-roles', title: 'Users Roles' },
+    { path: '/admin/roles-permissions', title: 'Roles Permissions' },
+    { path: '/admin/users-roles', title: 'Users Roles' },
+  ];
 
-  constructor(private observer: BreakpointObserver) {}
+  userRoutes = [
+    { path: '/user/dashboard', title: 'Dashboard' },
+    { path: '/user/profile', title: 'Profile' },
+    { path: '/user/settings', title: 'Settings' },
+  ];
+
+
+  constructor() {}
 
   ngOnInit() {
-    // this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
-    //   if(screenSize.matches){
-    //     this.isMobile = true;
-    //   } else {
-    //     this.isMobile = false;
-    //   }
-    // });
-  }
-
-  toggleMenu() {
-    // if(this.isMobile){
-    //   this.sidenav.toggle();
-    //   this.isCollapsed = false; // On mobile, the menu can never be collapsed
-    // } else {
-
-    // }
-    this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
-    // this.isCollapsed = !this.isCollapsed;
   }
 
 }
