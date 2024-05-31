3d4f54ba2141252202fd94718627f65f91d5058b
Kamran Moazim <kamrannaseer765@gmail.com>
2024-05-31 17:23:36 +0500

    created rating component

--- project/frontend/src/app/pages/event-details/event-details.component.ts
+++ project/frontend/src/app/pages/event-details/event-details.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Event } from '../../models/Event';
 import { Role } from '../../enums/role';
 import { User } from '../../models/User';
 import { Review } from '../../models/Review';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrl: './event-details.component.css'
 })
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit {
+
+
+  length = 50;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25];
+
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent: PageEvent;
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
+
 
   event:Event = {
     id: "1",
@@ -84,5 +112,45 @@ export class EventDetailsComponent {
       ...Array(halfStar).fill('star_half'),
       ...Array(5 - fullStars - halfStar).fill('star_border')
     ];
+  }
+
+
+  starRatings = [
+    { value: 1, icon: 'star' },
+    { value: 2, icon: 'star' },
+    { value: 3, icon: 'star' },
+    { value: 4, icon: 'star' },
+    { value: 5, icon: 'star' }
+  ];
+
+  ratingArr = [];
+  comments: string;
+  starCount: number = 5;
+  rating: number = 3;
+
+  submitReview() {
+    console.log('Rating:', this.rating);
+    console.log('Comments:', this.comments);
+    // Here you can send the review and comments to your backend or do any other processing.
+  }
+
+  ngOnInit() {
+    console.log("a "+this.starCount)
+    for (let index = 0; index < this.starCount; index++) {
+      this.ratingArr.push(index);
+    }
+  }
+  onClick(rating:number) {
+    console.log(rating)
+    this.rating = rating;
+    return false;
+  }
+
+  showIcon(index:number) {
+    if (this.rating >= index + 1) {
+      return 'star';
+    } else {
+      return 'star_border';
+    }
   }
 }
