577cb18693af92e68d007255ccd87b110dd3456c
Kamran Moazim <kamrannaseer765@gmail.com>
2024-05-31 17:38:53 +0500

    added form

--- project/frontend/src/app/pages/create-update-event/create-update-event.component.ts
+++ project/frontend/src/app/pages/create-update-event/create-update-event.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-update-event',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './create-update-event.component.css'
 })
 export class CreateUpdateEventComponent {
+
+  eventForm: FormGroup;
+  statusOptions = ['Pending', 'Approved', 'Cancelled'];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.eventForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      date: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+      location: ['', Validators.required],
+      capacity: ['', Validators.required],
+      category: ['', Validators.required],
+      ticketPrice: [''],
+      status: ['', Validators.required],
+      media: [null]
+    });
+  }
+
+  onSubmit() {
+    if (this.eventForm.valid) {
+      console.log(this.eventForm.value);
+      // Submit the event form data
+    }
+  }
 
 }
